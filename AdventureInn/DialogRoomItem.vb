d4057df12820bef86ab348b6777b381a40809423
PostalElf <thanthos@gmail.com>
2018-10-08 17:51:11 +0800

    Added Doubleclick Functionality to DialogRoomItem

--- AdventureInn/DialogRoomItem.vb
+++ AdventureInn/DialogRoomItem.vb
@@ -7,13 +7,17 @@
             ListBox1.Items.Add(entry.Name)
         Next
     End Sub
-
+    Private Sub ListBox1_DoubleClick(ByVal sender As Object, ByVal e As System.EventArgs) Handles ListBox1.DoubleClick
+        If ListBox1.SelectedItem Is Nothing Then Exit Sub
+        Me.DialogResult = Windows.Forms.DialogResult.OK
+    End Sub
     Private Sub Listbox1_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ListBox1.SelectedIndexChanged
         Dim i As Integer = ListBox1.SelectedIndex
         RoomItem = Inventory(i)
         lblDescription.Text = RoomItem.Description
         lblSize.Text = RoomItem.Width & "x" & RoomItem.Height
     End Sub
+
     Private Sub buttonOK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles buttonOK.Click
         Me.DialogResult = Windows.Forms.DialogResult.OK
     End Sub
