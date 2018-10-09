f1bbcad11edc1b2d71b25b0d7393ad28293ad6a9
PostalElf <thanthos@gmail.com>
2018-10-09 18:01:34 +0800

    Expanded RoomItem
    Added more traits

--- AdventureInn/Room/RoomItem.vb
+++ AdventureInn/Room/RoomItem.vb
@@ -12,6 +12,18 @@
                 Case "Width" : _Width = Convert.ToInt32(entry)
                 Case "Height" : _Height = Convert.ToInt32(entry)
                 Case "Description" : _Description = entry
+
+                Case "Furnishing" : Furnishing = Convert.ToInt32(entry)
+                Case "Opulence" : Opulence = Convert.ToInt32(entry)
+                Case "Tastefulness" : Opulence = Convert.ToInt32(-entry)
+                Case "Restfulness" : Restfulness = Convert.ToInt32(entry)
+                Case "Excitement" : Restfulness = Convert.ToInt32(-entry)
+                Case "Law", "Lawfulness" : Lawfulness = Convert.ToInt32(entry)
+                Case "Chaos" : Lawfulness = Convert.ToInt32(-entry)
+                Case "Faith" : Faith = Convert.ToInt32(entry)
+                Case "Strength" : Strength = Convert.ToInt32(entry)
+                Case "Focus" : Focus = Convert.ToInt32(entry)
+                Case "Tinkering" : Tinkering = Convert.ToInt32(entry)
             End Select
         Next
     End Sub
@@ -46,6 +58,64 @@
     Private Property InitialX As Integer Implements xy.InitialX
     Private Property InitialY As Integer Implements xy.InitialY
 
+    Private Furnishing As Integer
     Private Opulence As Integer
     Private Restfulness As Integer
+    Private Lawfulness As Integer
+    Private Faith As Integer
+    Private Strength As Integer
+    Private Focus As Integer
+    Private Tinkering As Integer
+    Public ReadOnly Property TotalFurnishing As String
+        Get
+            Select Case Furnishing
+                Case Is <= 0 : Return "Unfurnished"
+                Case 1 To 4 : Return "Barely Furnished"
+                Case Is >= 5 : Return "Well Furnished"
+                Case Else : Throw New Exception
+            End Select
+        End Get
+    End Property
+    Public ReadOnly Property TotalOpulence As String
+        Get
+            Select Case Opulence
+                Case 0 : Return "Boring"
+                Case Is < 0 : Return "Tasteful"
+                Case Is > 0 : Return "Opulent"
+                Case Else : Throw New Exception
+            End Select
+        End Get
+    End Property
+    Public ReadOnly Property TotalRestfulness As String
+        Get
+            Select Case Restfulness
+                Case 0 : Return "Middling"
+                Case Is < 0 : Return "Exciting"
+                Case Is > 0 : Return "Restful"
+                Case Else : Throw New Exception
+            End Select
+        End Get
+    End Property
+    Public ReadOnly Property TotalAlignment As String
+        Get
+            Select Case Lawfulness
+                Case 0 : Return "Neutral"
+                Case Is < 0 : Return "Chaotic"
+                Case Is > 0 : Return "Lawful"
+                Case Else : Throw New Exception
+            End Select
+        End Get
+    End Property
+    Public ReadOnly Property TotalNiche As String
+        Get
+            Dim highestNiche As String = ""
+            Dim highestValue As Integer = -1
+            If Faith > highestValue Then highestNiche = "Faith" : highestValue = Faith
+            If Strength > highestValue Then highestNiche = "Strength" : highestValue = Strength
+            If Focus > highestValue Then highestNiche = "Focus" : highestValue = Focus
+            If Tinkering > highestValue Then highestNiche = "Tinkering" : highestValue = Tinkering
+
+            Return highestNiche
+        End Get
+    End Property
 End Class
